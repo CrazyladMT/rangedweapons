5a411f29d5b461dc1d30db65e4082eb030e0496c
CrazyladMT <247920740+CrazyladMT@users.noreply.github.com>
2026-06-01 16:06:41 -0400

    Clean up `awp.lua`

--- awp.lua
+++ awp.lua
@@ -1,22 +1,35 @@
-
-
 core.register_tool("rangedweapons:awp_uld", {
 	stack_max= 1,
-	wield_scale = {x=1.9,y=1.9,z=1.1},
+	wield_scale = {x = 1.9, y = 1.9, z = 1.1},
 	range = 0,
-		description = "" ..core.colorize("#35cdff","A.W.P. \n") ..core.colorize("#FFFFFF", "Ranged damage:18 + 35/sec of bullet lifetime\n")..core.colorize("#FFFFFF", "Accuracy: 100%\n") ..core.colorize("#FFFFFF", "knockback: 15\n") ..core.colorize("#FFFFFF", "Critical chance: 30%\n") ..core.colorize("#FFFFFF", "Critical efficiency: x3\n") ..core.colorize("#FFFFFF", "Ammunition: 7.62mm round/308.Winchester rounds\n") ..core.colorize("#FFFFFF", "Rate of fire: 1.0\n") ..core.colorize("#FFFFFF", "Reload time: 2.0\n") ..core.colorize("#FFFFFF", "Zoom: 12x\n") ..core.colorize("#be0d00", "Right-click to eject empty bullet shell\n") ..core.colorize("#FFFFFF", "Clip size: 10/10\n") ..core.colorize("#FFFFFF", "Enemy penetration:30%\n") ..core.colorize("#FFFFFF", "Block penetration:10%\n") ..core.colorize("#FFFFFF", "Gun type: Rifle\n") ..core.colorize("#FFFFFF", "Bullet velocity: 75"),
+	description = core.colorize("#35cdff","A.W.P. \n") ..
+		"Ranged damage:18 + 35/sec of bullet lifetime\n" ..
+		"Accuracy: 100%\n" ..
+		"knockback: 15\n" ..
+		"Critical chance: 30%\n" ..
+		"Critical efficiency: x3\n" ..
+		"Ammunition: 7.62mm round/308.Winchester rounds\n" ..
+		"Rate of fire: 1.0\n" ..
+		"Reload time: 2.0\n" ..
+		"Zoom: 12x\n" ..
+		core.colorize("#be0d00", "Right-click to eject empty bullet shell\n") ..
+		"Clip size: 10/10\n" ..
+		"Enemy penetration:30%\n" ..
+		"Block penetration:10%\n" ..
+		"Gun type: Rifle\n" ..
+		"Bullet velocity: 75",
+
 	groups = {not_in_creative_inventory = 1},
 	inventory_image = "rangedweapons_awp.png",
 	weapon_zoom = 7.5,
 	on_use = function(itemstack, user)
 		core.sound_play("rangedweapons_empty", {pos = user:get_pos()}, true)
 	end,
-on_secondary_use = function(itemstack, user, pointed_thing)
-eject_shell(itemstack,user,"rangedweapons:awp_rld",1.0,"rangedweapons_rifle_reload_a","rangedweapons:empty_shell")
-return itemstack
-end,
+	on_secondary_use = function(itemstack, user, pointed_thing)
+		eject_shell(itemstack, user,"rangedweapons:awp_rld", 1.0, "rangedweapons_rifle_reload_a", "rangedweapons:empty_shell")
+		return itemstack
+	end,
 })
-
 
 core.register_tool("rangedweapons:awp_r", {
 	rw_next_reload = "rangedweapons:awp_rr",
@@ -27,6 +40,7 @@ core.register_tool("rangedweapons:awp_r", {
 	groups = {not_in_creative_inventory = 1},
 	inventory_image = "rangedweapons_awp_noclip.png",
 })
+
 core.register_tool("rangedweapons:awp_rr", {
 	rw_next_reload = "rangedweapons:awp_rrr",
 	load_sound = "rangedweapons_rifle_reload_a",
@@ -36,6 +50,7 @@ core.register_tool("rangedweapons:awp_rr", {
 	groups = {not_in_creative_inventory = 1},
 	inventory_image = "rangedweapons_awp.png",
 })
+
 core.register_tool("rangedweapons:awp_rrr", {
 	rw_next_reload = "rangedweapons:awp",
 	load_sound = "rangedweapons_rifle_reload_b",
@@ -45,7 +60,6 @@ core.register_tool("rangedweapons:awp_rrr", {
 	groups = {not_in_creative_inventory = 1},
 	inventory_image = "rangedweapons_awp_rld.png",
 })
-
 
 core.register_tool("rangedweapons:awp_rld", {
 	stack_max= 1,
@@ -58,19 +72,34 @@ core.register_tool("rangedweapons:awp_rld", {
 	inventory_image = "rangedweapons_awp_rld.png",
 })
 
-
 core.register_tool("rangedweapons:awp", {
-		description = "" ..core.colorize("#35cdff","A.W.P. \n") ..core.colorize("#FFFFFF", "Ranged damage:18 + 35/sec of bullet lifetime\n")..core.colorize("#FFFFFF", "Accuracy: 100%\n") ..core.colorize("#FFFFFF", "knockback: 15\n") ..core.colorize("#FFFFFF", "Critical chance: 30%\n") ..core.colorize("#FFFFFF", "Critical efficiency: x3\n") ..core.colorize("#FFFFFF", "Ammunition: 7.62mm round/308.Winchester rounds\n") ..core.colorize("#FFFFFF", "Rate of fire: 1.0\n") ..core.colorize("#FFFFFF", "Reload time: 2.0\n") ..core.colorize("#FFFFFF", "Zoom: 12x\n") ..core.colorize("#27a600", "The gun is loaded!\n") ..core.colorize("#FFFFFF", "Clip size: 10/10\n") ..core.colorize("#FFFFFF", "Enemy penetration:30%\n") ..core.colorize("#FFFFFF", "Block penetration:10%\n") ..core.colorize("#FFFFFF", "Gun type: Rifle\n") ..core.colorize("#FFFFFF", "Bullet velocity: 75"),
+	description = core.colorize("#35cdff","A.W.P. \n") ..
+		"Ranged damage:18 + 35/sec of bullet lifetime\n" ..
+		"Accuracy: 100%\n" ..
+		"knockback: 15\n" ..
+		"Critical chance: 30%\n" ..
+		"Critical efficiency: x3\n" ..
+		"Ammunition: 7.62mm round/308.Winchester rounds\n" ..
+		"Rate of fire: 1.0\n" ..
+		"Reload time: 2.0\n" ..
+		"Zoom: 12x\n" ..
+		core.colorize("#27a600", "The gun is loaded!\n") ..
+		"Clip size: 10/10\n" ..
+		"Enemy penetration:30%\n" ..
+		"Block penetration:10%\n" ..
+		"Gun type: Rifle\n" ..
+		"Bullet velocity: 75",
+
 	range = 0,
 	weapon_zoom = 7.5,
 	wield_scale = {x=1.9,y=1.9,z=1.1},
 	inventory_image = "rangedweapons_awp.png",
-RW_gun_capabilities = {
-		gun_damage = {fleshy=18,knockback=15},
+	RW_gun_capabilities = {
+		gun_damage = {fleshy = 18, knockback = 15},
 		gun_crit = 30,
 		gun_critEffc = 3.0,
-		suitable_ammo = {{"rangedweapons:762mm",10},{"rangedweapons:308winchester",10}},
-		gun_skill = {"rifle_skill",20},
+		suitable_ammo = {{"rangedweapons:762mm", 10},{"rangedweapons:308winchester", 10}},
+		gun_skill = {"rifle_skill", 20},
 		gun_magazine = "rangedweapons:rifle_mag",
 		gun_unloaded = "rangedweapons:awp_r",
 		gun_cooling = "rangedweapons:awp_uld",
@@ -89,13 +118,11 @@ RW_gun_capabilities = {
 		gun_sound = "rangedweapons_rifle_b",
 	},
 	on_secondary_use = function(itemstack, user, pointed_thing)
-rangedweapons_reload_gun(itemstack, user)
-return itemstack
-end,
-	on_use = function(itemstack, user, pointed_thing)
-rangedweapons_shoot_gun(itemstack, user)
-return itemstack
+		rangedweapons_reload_gun(itemstack, user)
+		return itemstack
 	end,
-
+	on_use = function(itemstack, user, pointed_thing)
+		rangedweapons_shoot_gun(itemstack, user)
+		return itemstack
+	end,
 })
-
