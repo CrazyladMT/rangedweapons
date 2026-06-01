d8b94e9f9bca7811ca0048343ee6ab0659365587
CrazyladMT <247920740+CrazyladMT@users.noreply.github.com>
2026-06-01 16:52:46 -0400

    Clean up `deagle.lua`

--- deagle.lua
+++ deagle.lua
@@ -1,18 +1,16 @@
-
-
-
 core.register_tool("rangedweapons:deagle_rld", {
 	stack_max= 1,
-	wield_scale = {x=1.25,y=1.25,z=1.5},
+	wield_scale = {x = 1.25, y = 1.25, z = 1.5},
 	description = "",
 	range = 0,
 	loaded_gun = "rangedweapons:deagle",
 	groups = {not_in_creative_inventory = 1},
 	inventory_image = "rangedweapons_deagle_rld.png",
 })
+
 core.register_tool("rangedweapons:deagle_r", {
 	stack_max= 1,
-	wield_scale = {x=1.25,y=1.25,z=1.5},
+	wield_scale = {x = 1.25, y = 1.25, z = 1.5},
 	description = "",
 	range = 0,
 	rw_next_reload = "rangedweapons:deagle",
@@ -22,13 +20,26 @@ core.register_tool("rangedweapons:deagle_r", {
 })
 
 core.register_tool("rangedweapons:deagle", {
-		description = "" ..core.colorize("#35cdff","Desert Eagle\n")..core.colorize("#FFFFFF", "Ranged damage: 11\n") ..core.colorize("#FFFFFF", "Accuracy: 85%\n") ..core.colorize("#FFFFFF", "knockback: 6\n")  ..core.colorize("#FFFFFF", "Critical chance: 20%\n") ..core.colorize("#FFFFFF", "Critical efficiency: 3x\n")..core.colorize("#FFFFFF", "Reload delay: 0.6\n")..core.colorize("#FFFFFF", "Clip size: 9/8/7\n")  ..core.colorize("#FFFFFF", "Ammunition: .357 Magnum rounds/.44 magnum rounds/.50AE catridges\n") ..core.colorize("#FFFFFF", "Rate of fire: 0.7\n") ..core.colorize("#FFFFFF", "Gun type: Magnum\n") ..core.colorize("#FFFFFF", "Block penetration: 5%\n")
-..core.colorize("#FFFFFF", "penetration: 15%\n")..core.colorize("#FFFFFF", "Bullet velocity: 50"),
-	wield_scale = {x=1.25,y=1.25,z=1.5},
+	description = core.colorize("#35cdff", "Desert Eagle\n") ..
+		"Ranged damage: 11\n" ..
+		"Accuracy: 85%\n" ..
+		"knockback: 6\n" ..
+		"Critical chance: 20%\n" ..
+		"Critical efficiency: 3x\n" ..
+		"Reload delay: 0.6\n" ..
+		"Clip size: 9/8/7\n" ..
+		"Ammunition: .357 Magnum rounds/.44 magnum rounds/.50AE catridges\n" ..
+		"Rate of fire: 0.7\n" ..
+		"Gun type: Magnum\n" ..
+		"Block penetration: 5%\n" ..
+		"penetration: 15%\n" ..
+		"Bullet velocity: 50",
+
+	wield_scale = {x = 1.25, y = 1.25, z = 1.5},
 	range = 0,
 	inventory_image = "rangedweapons_deagle.png",
-RW_gun_capabilities = {
-		gun_damage = {fleshy=11,knockback=6},
+	RW_gun_capabilities = {
+		gun_damage = {fleshy = 11, knockback = 6},
 		gun_crit = 20,
 		gun_critEffc = 2.2,
 		suitable_ammo = {{"rangedweapons:357",9},{"rangedweapons:44",8},{"rangedweapons:50ae",7}},
@@ -50,24 +61,25 @@ RW_gun_capabilities = {
 		gun_sound = "rangedweapons_deagle",
 	},
 	on_secondary_use = function(itemstack, user, pointed_thing)
-rangedweapons_reload_gun(itemstack, user)
-return itemstack
-end,
+		rangedweapons_reload_gun(itemstack, user)
+		return itemstack
+	end,
 	on_use = function(itemstack, user, pointed_thing)
-rangedweapons_shoot_gun(itemstack, user)
-return itemstack
+		rangedweapons_shoot_gun(itemstack, user)
+		return itemstack
 	end,
 })
 
 core.register_tool("rangedweapons:golden_deagle_rld", {
 	stack_max= 1,
-	wield_scale = {x=1.25,y=1.25,z=1.5},
+	wield_scale = {x = 1.25, y = 1.25, z = 1.5},
 	description = "",
 	range = 0,
 	loaded_gun = "rangedweapons:golden_deagle",
 	groups = {not_in_creative_inventory = 1},
 	inventory_image = "rangedweapons_golden_deagle_rld.png",
 })
+
 core.register_tool("rangedweapons:golden_deagle_r", {
 	stack_max= 1,
 	wield_scale = {x=1.25,y=1.25,z=1.5},
@@ -80,13 +92,25 @@ core.register_tool("rangedweapons:golden_deagle_r", {
 })
 
 core.register_tool("rangedweapons:golden_deagle", {
-		description = "" ..core.colorize("#35cdff","Golden Desert Eagle\n")..core.colorize("#FFFFFF", "Ranged damage: 14\n") ..core.colorize("#FFFFFF", "Accuracy: 90%\n") ..core.colorize("#FFFFFF", "knockback: 6\n")  ..core.colorize("#FFFFFF", "Critical chance: 23%\n") ..core.colorize("#FFFFFF", "Critical efficiency: 3x\n")..core.colorize("#FFFFFF", "Reload delay: 0.6\n")..core.colorize("#FFFFFF", "Clip size: 9/8/7\n")  ..core.colorize("#FFFFFF", "Ammunition: .357 Magnum rounds/.44 magnum rounds/.50AE catridges\n") ..core.colorize("#FFFFFF", "Rate of fire: 0.75\n") ..core.colorize("#FFFFFF", "Gun type: Magnum\n") ..core.colorize("#FFFFFF", "Block penetration: 5%\n")
-..core.colorize("#FFFFFF", "penetration: 15%\n")..core.colorize("#FFFFFF", "Bullet velocity: 50"),
-	wield_scale = {x=1.25,y=1.25,z=1.5},
+	description = core.colorize("#35cdff", "Golden Desert Eagle\n") ..
+		"Ranged damage: 14\n" ..
+		"Accuracy: 90%\n" ..
+		"knockback: 6\n" ..
+		"Critical chance: 23%\n" ..
+		"Critical efficiency: 3x\n" ..
+		"Reload delay: 0.6\n" ..
+		"Clip size: 9/8/7\n" ..
+		"Ammunition: .357 Magnum rounds/.44 magnum rounds/.50AE catridges\n" ..
+		"Rate of fire: 0.75\n" ..
+		"Gun type: Magnum\n" ..
+		"Block penetration: 5%\n" ..
+		"penetration: 15%\n" ..
+		"Bullet velocity: 50",
+	wield_scale = {x = 1.25, y = 1.25, z = 1.5},
 	range = 0,
 	inventory_image = "rangedweapons_golden_deagle.png",
-RW_gun_capabilities = {
-		gun_damage = {fleshy=14,knockback=6},
+	RW_gun_capabilities = {
+		gun_damage = {fleshy = 14, knockback = 6},
 		gun_crit = 23,
 		gun_critEffc = 2.2,
 		suitable_ammo = {{"rangedweapons:357",9},{"rangedweapons:44",8},{"rangedweapons:50ae",7}},
@@ -108,12 +132,12 @@ RW_gun_capabilities = {
 		gun_sound = "rangedweapons_deagle",
 	},
 	on_secondary_use = function(itemstack, user, pointed_thing)
-rangedweapons_reload_gun(itemstack, user)
-return itemstack
-end,
+		rangedweapons_reload_gun(itemstack, user)
+		return itemstack
+	end,
 	on_use = function(itemstack, user, pointed_thing)
-rangedweapons_shoot_gun(itemstack, user)
-return itemstack
+		rangedweapons_shoot_gun(itemstack, user)
+		return itemstack
 	end,
 })
 
